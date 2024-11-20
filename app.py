17c499cfd37d68353dcc4d87293646d2c71ea8d1
shanmuk <shanmukvinay454@gmail.com>
2024-11-20 18:44:55 +0530

    commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def load_data():
     # Map target to species names
     df['species'] = df['species'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
     return df
-
+#trail
 # Train and cache the model
 @st.cache_resource  # Cache the trained model so it's not retrained every time
 def train_model(df):
--- iris.py
+++ iris.py
@@ -19,6 +19,7 @@ y = df['species']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train RandomForest Classifier
+#model build
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
